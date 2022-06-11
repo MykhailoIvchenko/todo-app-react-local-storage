3e859fd4778684fa2e4137693eaf1e144a7097eb
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-06-11 23:15:41 +0300

    Clone todo-react-app-locale-storage

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+1. The project implements React application for tasks-management using locale storage of your computer.
+After first loading you'll see an input which allows to create new todo after pressing Enter key.
+The application lets you to add todos (and saves them in local storage).
+You also can filter todos (select all, completed and active). The number of completed todos are shown
+by the application interface. There is a checkbox which allows to make all todos completed and vice versa.
+It looks "active" if all todos are completed.
+Clicking at each separate todo makes it completed or active.
+Any todo can be removed by clicking "x" which appears on hover.
+There is an ability to clear completed todos - clicking on button "Clear completed" removes all completed items from the list.
+The button is visible if there is at least 1 completed item in the list.
+Everything except the input to add new todo are hidden if there are no todos. But not if todos are just filtered out.
+There is an inline editing for the TODO item. Double click on the TODO title makes it editable.
+Enter saves changes
+Esc cancels editing
+Todo title can't be empty! If you click Enter with empty todo editing input, this todo will be removed.
+If you edit todo and move focus out of the current todo, changes would be saved.
+
+2. Technologies stack: HTML5, CSS3, BEM, SASS (I was provided with ready markup and SCSS), JS, TS, React.
+
+3. Preview link: https:/mykhailoivchenko.github.io/todo-app-react/
+
+4. The prototype of the application is here: http://todomvc.com/examples/vanillajs/
+
+5. To add todo just write it into the field at the top of application. There is a version which
+uses Mate academy server API. Link to gh-pages: https:/mykhailoivchenko.github.io/todo-app-react/
+Link to repository https://github.com/MykhailoIvchenko/todo-app-react
---
+++ src/TodoApp.tsx
@@ -0,0 +1,227 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import NewTodoForm from './components/NewTodoForm/NewTodoForm';
+import TodoList from './components/TodoList/TodoList';
+import TodosCounter from './components/TodosCounter/TodosCounter';
+import TodosFilter from './components/TodosFilter/TodosFilter';
+import ClearCompletedButton
+  from './components/ClearCompletedButton/ClearCompletedButton';
+import ToggleAll from './components/ToggleAll/Toggle';
+import Filters from './enums/enums';
+
+let todoForSavingInLS: Todo[] = [];
+
+const TodoApp: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [toggleAllStatus, setToggleAllStatus] = useState(false);
+  const [currentFilter, setCurrentFilter] = useState(Filters.All);
+
+  todoForSavingInLS = [...todos];
+
+  const mainClasses = classNames({
+    main: true,
+    hidden: todos.length === 0,
+  });
+
+  const footerClasses = classNames({
+    footer: true,
+    hidden: todos.length === 0,
+  });
+
+  const editTodoTitle = (newTitle: string, todoId: number) => {
+    const changedArray = todos
+      .map(todo => {
+        if (todo.id === todoId) {
+          return {
+            id: todo.id,
+            title: newTitle,
+            completed: todo.completed,
+          };
+        }
+
+        return todo;
+      });
+
+    setTodos(changedArray.filter(todo => todo.title !== ''));
+  };
+
+  const editCompletedStatus = (newStatus: boolean, todoId: number) => {
+    const changedArray = todos
+      .map(todo => {
+        if (todo.id === todoId) {
+          return {
+            id: todo.id,
+            title: todo.title,
+            completed: newStatus,
+          };
+        }
+
+        return todo;
+      });
+
+    const isAnyTodoActive = changedArray.find(todo => todo.completed === false);
+
+    if (!isAnyTodoActive) {
+      setToggleAllStatus(true);
+    }
+
+    if (toggleAllStatus) {
+      setToggleAllStatus(false);
+    }
+
+    setTodos(changedArray);
+  };
+
+  const handleToggleAllClick = () => {
+    setTodos(prevTodos => prevTodos.map(todo => {
+      return {
+        id: todo.id,
+        title: todo.title,
+        completed: !toggleAllStatus,
+      };
+    }));
+
+    setToggleAllStatus(prev => !prev);
+  };
+
+  const handleFormSubmit
+    = (event: React.FormEvent<HTMLFormElement>, newTodoTitle: string) => {
+      event.preventDefault();
+      if (!newTodoTitle) {
+        return;
+      }
+
+      setTodos(prevTodos => [...prevTodos,
+        {
+          id: +new Date(),
+          title: newTodoTitle,
+          completed: false,
+        }]);
+    };
+
+  const applyFilter = (chosenFilter: string) => {
+    switch (chosenFilter) {
+      case Filters.Active:
+        setCurrentFilter(Filters.Active);
+        break;
+      case Filters.Completed:
+        setCurrentFilter(Filters.Completed);
+        break;
+      default:
+        setCurrentFilter(Filters.All);
+    }
+  };
+
+  const getVisibleTodos = () => {
+    switch (currentFilter) {
+      case Filters.Active:
+        return todos.filter(todo => todo.completed === false);
+      case Filters.Completed:
+        return todos.filter(todo => todo.completed === true);
+      default:
+        return todos;
+    }
+  };
+
+  const getActive = () => {
+    const changedArray = todos.filter(todo => !todo.completed);
+
+    setTodos(changedArray);
+
+    return changedArray.length;
+  };
+
+  const handleClearCompletedClick = () => {
+    const numberOfActiveTodos = getActive();
+
+    if (numberOfActiveTodos === 0) {
+      setToggleAllStatus(prev => !prev);
+    }
+  };
+
+  const getCompletedTodosLength = () => {
+    return todos.filter(todo => todo.completed).length;
+  };
+
+  const handleWindowClose = () => {
+    const todosStringForLS = JSON.stringify(todoForSavingInLS);
+
+    localStorage.setItem('todos', todosStringForLS);
+  };
+
+  useEffect(() => {
+    const todosStringFromLS = localStorage.getItem('todos') || '';
+    let todosFromLS: Todo[] = [];
+
+    if (todosStringFromLS) {
+      todosFromLS = JSON.parse(todosStringFromLS);
+    }
+
+    if (todosFromLS.length > 0) {
+      setTodos([...todosFromLS]);
+
+      const numberOfActiveTodos = todosFromLS
+        .filter(todo => !todo.completed).length;
+
+      if (numberOfActiveTodos === 0) {
+        setToggleAllStatus(true);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('beforeunload',
+      handleWindowClose);
+
+    return () => {
+      window.removeEventListener('beforeunload',
+        handleWindowClose);
+    };
+  }, []);
+
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+
+        <NewTodoForm
+          data-cy="createTodo"
+          handleSubmit={handleFormSubmit}
+        />
+      </header>
+
+      <section className={mainClasses}>
+        <ToggleAll
+          toggleAllStatus={toggleAllStatus}
+          handleToggleAllClick={handleToggleAllClick}
+        />
+
+        <TodoList
+          data-cy="todoList"
+          items={getVisibleTodos()}
+          editTodoTitle={editTodoTitle}
+          editCompletedStatus={editCompletedStatus}
+        />
+      </section>
+
+      <footer className={footerClasses}>
+        <TodosCounter
+          data-cy="todosCounter"
+          todos={todos}
+        />
+
+        <TodosFilter
+          applyFilter={applyFilter}
+        />
+
+        <ClearCompletedButton
+          completedTodosLength={getCompletedTodosLength()}
+          handleClearCompletedClick={handleClearCompletedClick}
+        />
+      </footer>
+    </section>
+  );
+};
+
+export default TodoApp;
